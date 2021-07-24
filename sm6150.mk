4757eb283fe2b91131d4c044b7f66fa1f9ac8d8d
xNombre <kartapolska@gmail.com>
2021-07-24 21:54:40 +0200

    sm6150-common: Build MuiuiCamera as default camera app
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Change-Id: Ic197fd241fd4fa5b0924ede9d397c8800d9daf52

--- sm6150.mk
+++ sm6150.mk
@@ -100,14 +100,13 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
     libdng_sdk.vendor
 
-PRODUCT_PACKAGES += \
-    Snap
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
+
+$(call inherit-product, vendor/miuicamera/config.mk)
 
 # Device-specific settings
 PRODUCT_PACKAGES += \
