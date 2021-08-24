64b6ca4ac52061014888cae6ec944b1052f444c0
xNombre <kartapolska@gmail.com>
2021-08-24 17:18:21 +0200

    sm6150-common: Nuke soundtrigger completely
    Breaks Ok google...
    
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Change-Id: Ibc15a2b42791967d5a91e1e23b1a0e9209792553

--- sm6150.mk
+++ sm6150.mk
@@ -41,8 +41,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.0-impl:32 \
-    android.hardware.soundtrigger@2.2-impl:32
+    android.hardware.bluetooth.audio@2.0-impl:32
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
@@ -61,9 +60,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
