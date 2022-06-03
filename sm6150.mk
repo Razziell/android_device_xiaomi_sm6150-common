6662aeffdefb49719f5e1fe0d144bfa8a0342af2
Andrzej Perczak <linux@andrzejperczak.com>
2022-06-04 00:55:35 +0200

    sm6150-common: Sound trigger? No thank you
    Sound trigger is broken and causes problems with Ok Google. Drop it
    altogether. Maybe some day a better fix will be found...
    
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>

--- sm6150.mk
+++ sm6150.mk
@@ -47,8 +47,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.1-impl \
-    android.hardware.soundtrigger@2.2-impl
+    android.hardware.bluetooth.audio@2.1-impl
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
@@ -67,9 +66,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
